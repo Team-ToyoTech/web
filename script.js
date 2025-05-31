924d79b370054cb7fae1bd388710262fb5401e16
DH.L <meozigoon@gmail.com>
2025-05-31 12:48:54 +0900

    v2.2.0-a
    프로필 정보 업데이트 및 스타일 수정

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ const profiles = {
                 lee: {
                     title: "이동하 Dongha Lee",
                     content:
-                        "한성과고 34기, 과학기술정보통신부 장관상 수상, 단대대소고 프로그래밍 대회 은상 수상, BOJ 플레티넘 3 달성",
+                        `한성과고 34기, 2009년생, 
+                        과학기술정보통신부 장관상 수상, 단대대소고 프로그래밍 대회 은상 수상, 
+                        가능 프로그래밍 언어 C, C++, C#, Python, JavaScript. 
+                        ToyoTech에서는 프로젝트 코딩 및 펌웨어 개발, 프론트엔드 개발, 일부 PCB 디자인 등을 담당, 
+                        BOJ 플레티넘 3 달성`,
                 },
                 park: {
                     title: "박준현 Junhyun Park",
                     content:
-                        "이대부고 68기, 삼성 주니어 SW 창작 대회 공감상 수상, BOJ 플래티넘 5 달성",
+                        `이대부고 68기, 2009년생, 
+                        삼성 주니어 SW 창작 대회 공감상 수상, 
+                        가능 프로그래밍 언어 C, C++, C#, Python, Java. 
+                        ToyoTech에서는 프로젝트 프로그래밍, 백엔드 개발 등을 담당, 
+                        BOJ 플래티넘 5 달성`,
                 },
                 kim: {
                     title: "김주원 Juwon Kim",
-                    content: "성산중",
+                    content: 
+                        `성산중 2학년, 2011년생, 
+                        가능 프로그래밍 언어 C, C++, C#. 
+                        ToyoTech에서는 펌웨어 개발, 납땜 등을 담당, 
+                        BOJ 골드 3 달성`,
                 },
             };
 
@@ -29,15 +41,9 @@ const profiles = {
                         const name = member.getAttribute("data-name");
                         const profile = profiles[name];
                         if (profile) {
-                            document.getElementById(
-                                "profileTitle"
-                            ).textContent = profile.title;
-                            document.getElementById(
-                                "profileContent"
-                            ).textContent = profile.content;
-                            document.getElementById(
-                                "profileModal"
-                            ).style.display = "block";
+                            document.getElementById("profileTitle").textContent = profile.title;
+                            document.getElementById("profileContent").innerHTML = profile.content.replace(/\n/g, "<br>");
+                            document.getElementById("profileModal").style.display = "block";
                         }
                     });
                 });
