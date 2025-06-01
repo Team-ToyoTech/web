600f32ff90ccd92a68b4036e0b8ae966c3b3d37c
DH.L <meozigoon@gmail.com>
2025-06-01 12:51:39 +0900

    v3.3.3-a
    프로필 내용 수정: ToyoTech 담당 업무 설명을 명확히 함

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const profiles = {
         content: 
             `도쿄이과대학 창역이공학부 전기전자정보공학과 25학번, 2006년생. 
             가능 프로그래밍 언어 C, C++, C#, Python, JavaScript, Java.
-            ToyoTech에서는 PCB 디자인, 프로젝트 아이디어, 일부 프로젝트 코딩 등을 담당.
+            ToyoTech에서는 PCB 디자인, 펌웨어 개발, 일부 프로젝트 프로그래밍밍 담당.
             ToyoTech의 학생들을 직접 가르침.`,
     },
     lee: {
@@ -13,7 +13,7 @@ const profiles = {
             `한성과고 34기, 2009년생, 
             과학기술정보통신부 장관상 수상, 단대대소고 프로그래밍 대회 은상 수상, 
             가능 프로그래밍 언어 C, C++, C#, Python, JavaScript. 
-            ToyoTech에서는 프로젝트 코딩 및 펌웨어 개발, 프론트엔드 개발, 일부 PCB 디자인 등을 담당, 
+            ToyoTech에서는 프로젝트 코딩 및 펌웨어 개발, 프론트엔드 개발, 일부 PCB 디자인 담당, 
             BOJ 플레티넘 3 달성`,
     },
     park: {
@@ -22,7 +22,7 @@ const profiles = {
             `이대부고 68기, 2009년생, 
             삼성 주니어 SW 창작 대회 공감상 수상, 
             가능 프로그래밍 언어 C, C++, C#, Python, Java. 
-            ToyoTech에서는 프로젝트 프로그래밍, 백엔드 개발 등을 담당, 
+            ToyoTech에서는 프로젝트 프로그래밍, 백엔드 개발 담당, 
             BOJ 플래티넘 5 달성`,
     },
     kim: {
@@ -30,7 +30,7 @@ const profiles = {
         content: 
             `성산중 2학년, 2011년생, 
             가능 프로그래밍 언어 C, C++, C#. 
-            ToyoTech에서는 펌웨어 개발, 납땜 등을 담당, 
+            ToyoTech에서는 펌웨어 개발, 납땜 담당, 
             BOJ 골드 3 달성`,
     },
 };
